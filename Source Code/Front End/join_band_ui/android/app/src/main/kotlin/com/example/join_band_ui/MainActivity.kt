f63575b0d1f9c50d6fa9a3119340cd35dfe3e573
kagfupanda <kagfupanda@gmail.com>
2021-02-28 15:51:28 -0800

    added join band ui

---
+++ Source Code/Front End/join_band_ui/android/app/src/main/kotlin/com/example/join_band_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.join_band_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Source Code/Front End/join_band_ui/lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+
+String bandId;
+String userId;
+
+TextEditingController userIdFromText = new TextEditingController();
+TextEditingController bandIdFromText = new TextEditingController();
+
+void main() {
+  runApp(JoinBand());
+}
+
+class JoinBand extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Join Band",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Join a Band"),
+        ),
+        body: new GetTextFieldValue()
+        )
+      );
+}
+}
+
+class GetTextFieldValue extends StatefulWidget {
+ 
+  BandIDWidget createState() => BandIDWidget();
+ 
+}
+
+class BandIDWidget extends State<GetTextFieldValue> {
+  @override
+  Widget build(BuildContext context) {
+
+    setIds() {
+      setState(() {
+      userId = userIdFromText.text;
+      bandId = bandIdFromText.text;
+      userIdFromText.clear();
+      bandIdFromText.clear();
+      print("User Id is: " + userId);
+      print("Band Id is : " + bandId);
+      });
+    }
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          new Text(
+            "Enter User and Band ID",
+            style: new TextStyle(fontSize: 24.0),
+          ), 
+          new Container(
+            width: 200,
+            child: new TextField(
+              controller: userIdFromText,
+              decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'User ID',
+              ),
+            ),
+          ),
+          new Container(
+            width:200,
+            child: new TextField(
+              controller: bandIdFromText,
+              decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Band ID',
+              ),
+            ),
+          ),
+          new ElevatedButton(
+            onPressed: setIds,
+            child: new Text("Submit")
+            )
+        ]
+      )
+    );
+  }
+}
+
+getUserID() {
+  return userId;
+}
+
+getBandId() {
+  return bandId;
+}
+
